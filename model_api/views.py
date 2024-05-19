25c1808c5ba76d526c07a2c23016dd4a0c441e61
Your Name <your.email@example.com>
2024-05-19 05:32:15 +0300

    trained models with higher epochs

--- model_api/views.py
+++ model_api/views.py
@@ -1,35 +1,52 @@
+# views.py
+import json
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-
 from ultralytics import YOLO
 from PIL import Image
 from io import BytesIO
-# Load your model
-model = YOLO('./cigarette_model1.pt')
+import os
 
+# Define paths to your models
+main_dir = os.path.dirname(os.path.realpath(__file__))
+modelcigarette_path = os.path.join(main_dir, "best.pt")
+modelgun_path = os.path.join(main_dir, "gun_model.pt")
+
+# Load your models
+modelcigarette = YOLO(modelcigarette_path)
+modelgun = YOLO(modelgun_path)
+
+def process_image(image):
+    try:
+        result1 = modelcigarette.predict(image, classes=0, conf=0.70, augment=True)
+        boxes_cigarette = result1[0].boxes
+        result2 = modelgun.predict(image, classes=0, conf=0.60, augment=True)
+        boxes_gun = result2[0].boxes
+
+        if boxes_gun and boxes_cigarette:
+            return {'gun': True, 'cigarette': True}
+        elif boxes_gun:
+            return {'gun': True, 'cigarette': False}
+        elif boxes_cigarette:
+            return {'gun': False, 'cigarette': True}
+        else:
+            return {'cigarette': False, 'gun': False}
+    except Exception as e:
+        return {'error': str(e)}
 
 @method_decorator(csrf_exempt, name='dispatch')
 class PredictView(View):
     def post(self, request, *args, **kwargs):
-        # Check if the 'image' key exists in request.FILES
         if 'image' not in request.FILES:
             return HttpResponseBadRequest('Image file not provided')
 
-        # Get the image from the request
         image_data = request.FILES['image'].read()
-
-        # Convert the image data to a PIL Image object
         image = Image.open(BytesIO(image_data))
 
-        # Use your model for inference
-        prediction = model.predict(image, conf=0.10, classes=0, augment=True)
-        boxes = prediction[0].boxes
-        if boxes:
-            return JsonResponse({'prediction': True})
-        else:
-            return JsonResponse({'prediction': False})
+        result = process_image(image)
+        return JsonResponse(result)
 
     def get(self, request, *args, **kwargs):
         return JsonResponse({'message': 'This is a GET request'})
